83fedab6d8ee5d60909a20715be004a0e330515c
Marcel Benzinger <benzinger.marcel@gmail.com>
2023-12-19 10:22:33 +0100

    Add access control and fields to Datenschutz collection

--- src/collections/Datenschutz.ts
+++ src/collections/Datenschutz.ts
@@ -1,10 +1,49 @@
 import { CollectionConfig } from "payload/types";
+import { isAdminOrEditorOrPublished } from "../access/isAdminOrEditorOrPublished";
+import { isAdminOrEditor } from "../access/isAdminOrEditor";
 
-const Datenschutz: CollectionConfig = {
+export const Datenschutz: CollectionConfig = {
   slug: "datenschutz",
-  auth: true,
-  admin: {},
-  fields: [],
+  admin: {
+    useAsTitle: "title",
+  },
+  access: {
+    create: isAdminOrEditor,
+    update: isAdminOrEditor,
+    read: isAdminOrEditorOrPublished,
+    delete: isAdminOrEditor,
+  },
+  fields: [
+    {
+      name: "datenschutz",
+      type: "group",
+      fields: [
+        {
+          name: "title",
+          label: "Title",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "autor",
+          label: "Autor",
+          type: "relationship",
+          relationTo: "users",
+          required: true,
+        },
+        {
+          name: "datum",
+          label: "Datum",
+          type: "date",
+          required: true,
+        },
+        {
+          name: "content",
+          label: "Inhalt",
+          type: "richText",
+          required: true,
+        },
+      ],
+    },
+  ],
 };
-
-export default Datenschutz;
