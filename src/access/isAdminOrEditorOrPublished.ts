94cbc302a4b7dbe895182250a649809d8bfc4a43
Marcel Benzinger <benzinger.marcel@gmail.com>
2023-12-19 10:22:06 +0100

    Add isAdminOrEditorOrPublished access function

---
+++ src/access/isAdminOrEditorOrPublished.ts
@@ -0,0 +1,19 @@
+import { Access } from "payload/config";
+
+export const isAdminOrEditorOrPublished: Access = ({ req: { user } }) => {
+  // Need to be logged in
+  if (user) {
+    // If user has role of 'admin'
+    if (user.roles.includes("admin")) return true;
+
+    // If user has role of 'editor'
+    if (user.roles.includes("editor")) return true;
+  }
+
+  // Non-logged in users can only read published docs
+  return {
+    _status: {
+      equals: "published",
+    },
+  };
+};
