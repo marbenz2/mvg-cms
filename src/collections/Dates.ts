b5d783e06fc96ec63413f3eafab7a9fa85b19d66
Marcel Benzinger <benzinger.marcel@gmail.com>
2023-12-19 10:22:39 +0100

    Add access control and fields to Dates collection

--- src/collections/Dates.ts
+++ src/collections/Dates.ts
@@ -1,10 +1,49 @@
 import { CollectionConfig } from "payload/types";
+import { isAdminOrEditorOrPublished } from "../access/isAdminOrEditorOrPublished";
+import { isAdminOrEditor } from "../access/isAdminOrEditor";
 
-const Dates: CollectionConfig = {
+export const Dates: CollectionConfig = {
   slug: "dates",
-  auth: true,
-  admin: {},
-  fields: [],
+  admin: {
+    useAsTitle: "title",
+  },
+  access: {
+    create: isAdminOrEditor,
+    update: isAdminOrEditor,
+    read: isAdminOrEditorOrPublished,
+    delete: isAdminOrEditor,
+  },
+  fields: [
+    {
+      name: "dates",
+      type: "group",
+      fields: [
+        {
+          name: "title",
+          label: "Title",
+          type: "text",
+          required: true,
+        },
+        {
+          name: "autor",
+          label: "Autor",
+          type: "relationship",
+          relationTo: "users",
+          required: true,
+        },
+        {
+          name: "datum",
+          label: "Datum",
+          type: "date",
+          required: true,
+        },
+        {
+          name: "content",
+          label: "Content",
+          type: "text",
+          required: true,
+        },
+      ],
+    },
+  ],
 };
-
-export default Dates;
