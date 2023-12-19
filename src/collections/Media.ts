e47e9bf92dbcfd5842c0d5da993a2920229307e5
Marcel Benzinger <benzinger.marcel@gmail.com>
2023-12-19 10:22:50 +0100

    Refactor access control in Media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,20 +1,15 @@
 import { CollectionConfig } from "payload/types";
-import { isAdmin } from "../access/isAdmin";
-import { isAdminOrHasSiteAccess } from "../access/isAdminOrHasSiteAccess";
-import { isLoggedIn } from "../access/isLoggedIn";
+import { isAdminOrEditorOrPublished } from "../access/isAdminOrEditorOrPublished";
+import { isAdminOrEditor } from "../access/isAdminOrEditor";
 
 export const Media: CollectionConfig = {
   slug: "media",
   upload: true,
   access: {
-    // Anyone logged in can create
-    create: isLoggedIn,
-    // Only admins or editors with site access can update
-    update: isAdminOrHasSiteAccess(),
-    // Only admins or editors with site access can read
-    read: isAdminOrHasSiteAccess(),
-    // Only admins or editors with site access can delete
-    delete: isAdminOrHasSiteAccess(),
+    create: isAdminOrEditor,
+    update: isAdminOrEditor,
+    read: isAdminOrEditorOrPublished,
+    delete: isAdminOrEditor,
   },
   fields: [
     {
@@ -22,18 +17,5 @@ export const Media: CollectionConfig = {
       type: "text",
       required: true,
     },
-    /*     {
-      name: "site",
-      type: "relationship",
-      relationTo: "sites",
-      required: true,
-      // If user is not admin, set the site by default
-      // to the first site that they have access to
-      defaultValue: ({ user }) => {
-        if (!user.roles.includes("admin") && user.sites?.[0]) {
-          return user.sites[0];
-        }
-      },
-    }, */
   ],
 };
